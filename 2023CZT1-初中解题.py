756186f8fc8f68c95e1fe4a68ea9f92b46f23730
61758084 <108806281+61758084@users.noreply.github.com>
2024-11-29 14:35:24 +0800

    Create 2023CZT1-初中解题.py

---
+++ 2023CZT1-初中解题.py
@@ -0,0 +1,24 @@
+a = input()
+a = int(a)
+if a == 1:
+    print('B')
+elif a == 2:
+    print('B')
+elif a == 3:
+    print('D')
+elif a == 4:
+    print('B')
+elif a == 5:
+    print(54548)
+elif a == 6:
+    print('A')
+elif a == 7:
+    print(11000011)
+elif a == 8:
+    print('B')
+elif a == 9:
+    print('B')
+elif a == 10:
+    print('C')
+elif a > 10:
+    print('Wrong')
